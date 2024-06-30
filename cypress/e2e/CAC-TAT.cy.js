573b99603a656ed879cd4214cfc1ca9ef892a86a
tatyyukie <tatyy-yukie@hotmail.com>
2024-06-30 10:07:05 -0300

    Validação dos campos e-mail e telefone

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -27,4 +27,24 @@ describe("Central de Atendimento ao Cliente TAT", function () {
         cy.get('button[type="submit"]').click();
         cy.get(".success").should("be.visible");
     });
+
+    it("Exibe mensagem de erro ao submeter o formulário com um email com formatação inválida", function () {
+        cy.get("#firstName").type("Tatiana").should("have.value", "Tatiana");
+        cy.get("#lastName").type("Motoyama").should("have.value", "Motoyama");
+        cy.get("#email")
+            .type("tatiana@motoyama,com")
+            .should("have.value", "tatiana@motoyama,com");
+        cy.get("#phone")
+            .type("11999999999")
+            .should("have.value", "11999999999");
+        cy.get("#open-text-area").type("Teste").should("have.value", "Teste");
+        cy.get('button[type="submit"]').click();
+        cy.get(".error").should("be.visible");
+    });
+
+    it.only("Campo telefone continua vazio quando preenchido com valor não-numérico", function () {
+        cy.get("#phone")
+            .type("abcdefghijklmnopqrstuvwxyz")
+            .should("have.value", "");
+    });
 });

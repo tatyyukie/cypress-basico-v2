e50c00102b5a9bca27e2c3a447c95db4060424f0
tatyyukie <tatyy-yukie@hotmail.com>
2024-06-30 23:27:59 -0300

    Marca o tipo de atendimento Feedback e verifica se o valor correto foi selecionado

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -97,7 +97,13 @@ describe("Central de Atendimento ao Cliente TAT", function () {
         cy.get("#product").select("YouTube").should("have.value", "youtube");
     });
 
-    it.only("Seleciona um produto (Blog) por seu índice", function () {
+    it("Seleciona um produto (Blog) por seu índice", function () {
         cy.get("#product").select(1).should("have.value", "blog");
+    });
+
+    it("Marca o tipo de atendimento Feedback e verifica se o valor correto foi selecionado", function () {
+        cy.get('input [type="radio"], [value="feedback"]')
+            .check()
+            .should("have.value", "feedback")
     });
 });

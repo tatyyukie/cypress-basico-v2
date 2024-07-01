1f321a89ef4bd0978675920d4286483a5150ef11
tatyyukie <tatyy-yukie@hotmail.com>
2024-07-01 19:32:26 -0300

    Inclusão para gravação dos testes

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,7 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
   viewportHeight: 768,
   viewportWidth: 1280,
+  video: false,
   e2e: {
     setupNodeEvents(on, config) {},
   }
